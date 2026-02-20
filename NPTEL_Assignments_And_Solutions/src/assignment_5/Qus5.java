73d15b56eaa0803c549b90c9bc8c2113195235c5
Rajnish Chauhan <crajnish425@email.com>
2026-02-20 21:33:40 +0530

    Assignment 5 Qus 5

---
+++ NPTEL_Assignments_And_Solutions/src/assignment_5/Qus5.java
@@ -0,0 +1,28 @@
+package assignment_5;
+interface Calculator {
+    void calculate(int value);
+}
+
+class Square implements Calculator {
+    int result;
+
+    public void calculate(int value) {
+        result = value * value;
+        System.out.print("Square: " + result + " ");
+    }
+}
+
+class Cube extends Square {
+    public void calculate(int value) {
+        result = value * value * value;
+        super.calculate(value);
+        System.out.print("Cube: " + result + " ");
+    }
+}
+
+public class Qus5 {
+    public static void main(String[] args) {
+        Calculator obj = new Cube();
+        obj.calculate(3);
+    }
+}
